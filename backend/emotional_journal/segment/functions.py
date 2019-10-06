827c0b9f9898041eac99ffaa255ef32582136c81
krishtna999 <krishtna999@gmail.com>
2019-10-07 03:21:12 +0530

    Added changes to functions.py

--- backend/emotional_journal/segment/functions.py
+++ backend/emotional_journal/segment/functions.py
@@ -1,19 +1,31 @@
 from tag.models import EventTag, SegmentTag
 from .models import Segment, Event
 
+
 def create_events(events, segment):
     for event in events:
-        new_event = Event.create(
-            event['start_index'], event['end_index'], segment)
-        
-        create_eventTags(event['tags'],new_event)
+        new_event = Event.objects.create(
+            start_index=event['start_index'], end_index=event['end_index'], segment=segment)
+
+        create_eventTags(event['tags'], new_event)
 
 
 def create_eventTags(tags, event):
     for tag in tags:
-        EventTag.create(tag['type'],tag['name'],tag['note'],event)
+        if('note' in tag):
+            EventTag.objects.create(
+                type=tag['type'], name=tag['name'], note=tag['note'], event=event)
+        else:
+            EventTag.objects.create(
+                type=tag['type'], name=tag['name'], event=event)
 
 
 def create_segmentTags(tags, segment):
     for tag in tags:
-        Segment.create(tag['type'],tag['name'],segment)
+        SegmentTag.objects.create(type=tag['type'],name=tag['name'],segment=segment)
+
+def retrieve_event_text(event):
+    retrieve_text=event.segment.text[event.start_index:event.end_index]
+    return retrieve_text
+
+    
