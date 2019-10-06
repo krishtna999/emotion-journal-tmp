3f73b4acd98f48fa3d018c088a41d4656d566ebb
krishtna999 <krishtna999@gmail.com>
2019-10-07 04:24:40 +0530

    Added list urls

--- backend/emotional_journal/segment/urls.py
+++ backend/emotional_journal/segment/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.SegmentCreate.as_view(), name='Create New Segment'),
-    path('event/create/', views.EventCreate.as_view(), name='Create New Event'),
+    path('create', views.SegmentCreate.as_view(), name='Create New Segment'),
+    path('list', views.SegmentList.as_view(), name='List Events'),
+    path('event/create', views.EventCreate.as_view(), name='Create New Event'),
+    path('event/list', views.EventList.as_view(), name='List Events'),
+
     
 ]
