c2954dd28bca9973b864308f7027584ee3412683
krishtna999 <krishtna999@gmail.com>
2019-10-07 15:00:38 +0530

    Added tag urls to main urls file

--- backend/emotional_journal/emotion_journal/urls.py
+++ backend/emotional_journal/emotion_journal/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api/segment/',include('segment.urls')),
+    path('api/tag/',include('tag.urls')),
 
     
 ]
