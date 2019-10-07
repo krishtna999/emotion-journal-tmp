feac9d402f95b9c83c3f8d799f3f72a2eb9f3cf3
krishtna999 <krishtna999@gmail.com>
2019-10-07 15:05:39 +0530

    Added CRUD using ModelViewSet for Segment and Event

--- backend/emotional_journal/segment/urls.py
+++ backend/emotional_journal/segment/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create', views.SegmentCreate.as_view(), name='Create New Segment'),
-    path('list', views.SegmentList.as_view(), name='List Events'),
-    path('event/create', views.EventCreate.as_view(), name='Create New Event'),
-    path('event/list', views.EventList.as_view(), name='List Events'),
+    path('create/', views.SegmentCreate.as_view(), name='Create New Segment'),
+    path('', views.SegmentViewSet.as_view({'get': 'list'})),
+    path('<int:pk>/', views.SegmentViewSet.as_view(
+        {'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'})),
 
-    
+
+    path('event/create/', views.EventCreate.as_view(), name='Create New Event'),
+    path('event/', views.EventViewSet.as_view({'get': 'list'})),
+    path('event/<int:pk>/', views.EventViewSet.as_view(
+        {'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'})),
 ]
