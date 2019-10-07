dcdf36d10f2d2ee5cb6acfcf41cb2ecc81020a85
krishtna999 <krishtna999@gmail.com>
2019-10-07 15:10:45 +0530

    Replaced generics with ViewSets

--- backend/emotional_journal/tag/views.py
+++ backend/emotional_journal/tag/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters import rest_framework as filters
 
-# Create your views here.
+from .models import EventTag,SegmentTag
+from .serializers import EventTagSerializer,SegmentTagSerializer
+from .filters import EventTagFilter,SegmentTagFilter
+
+class EventViewSet(viewsets.ModelViewSet):
+    serializer_class=EventTagSerializer
+    queryset=EventTag.objects.all()
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = EventTagFilter
+
+class SegmentViewSet(viewsets.ModelViewSet):
+    serializer_class=SegmentTagSerializer
+    queryset=SegmentTag.objects.all()
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = SegmentTagFilter
