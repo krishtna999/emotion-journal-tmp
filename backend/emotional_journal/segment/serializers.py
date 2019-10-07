bb3d41323a7363b637cbe1048fefb07f1357078f
krishtna999 <krishtna999@gmail.com>
2019-10-07 15:03:06 +0530

    Updated serializers

--- backend/emotional_journal/segment/serializers.py
+++ backend/emotional_journal/segment/serializers.py
@@ -4,17 +4,19 @@ from .models import Event,Segment
 
 class EventSerializer(serializers.ModelSerializer):
     event_text=serializers.CharField(source='retrieve_event_text')
+    segment_id=serializers.IntegerField(source='segment.id')
     class Meta:
         model = Event
-        fields = ['tags','event_text']
+        fields = ['id','segment_id','start_index','end_index','event_text','tags']
+        read_only=('segment_id','event_text')
+        # write_only=('start_index','end_index')
         depth=1
 
 
 class SegmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Segment
-        fields = ['text','datetime']
+        fields = ['id','text','datetime','tags']
         depth=1
 
 
- 
