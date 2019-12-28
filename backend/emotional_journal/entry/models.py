f36b13a14925c720f96fa47fcae0c4f028c9b4f2
krishtna999 <krishtna999@gmail.com>
2019-12-29 03:00:19 +0530

    Added TODO to models.py

--- backend/emotional_journal/entry/models.py
+++ backend/emotional_journal/entry/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from datetime import datetime
-from .choices import SEGMENT_TYPE_CHOICES
 # Create your models here.
 
 
@@ -9,7 +8,14 @@ class Entry(models.Model):
     
 
 class Event(models.Model):
-    text=models.TextField()
+    # Emotional Text entry:
+    text=models.TextField(null=False)
+
+    '''
+    TODO: Decide if "Rational Text entry" is needed as a feature or not
+    text_rat=models.TextField(null=True)
+    '''
+    
     # order_id is going to be how the events are going to be ordered and displayed
     order_id = models.CharField(max_length=12)
     entry = models.ForeignKey(
