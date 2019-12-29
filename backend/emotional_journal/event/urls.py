831f0222a29dba2007bc12dacb8e20b59dfe204e
krishtna999 <krishtna999@gmail.com>
2019-12-29 18:20:45 +0530

    Renamed tag-entry endpoint to "tag" from "" (empty string)

--- backend/emotional_journal/event/urls.py
+++ backend/emotional_journal/event/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     # TODO: Add functionality for PARTIAL_UPDATE. Check views file
-    path('', views.EventCreate.as_view(), name='Create New Event'),
-    path('list/', views.EventViewSet.as_view({'get': 'list'})),
+    path('tag/', views.EventCreate.as_view(), name='Create New Event'),
+    path('', views.EventViewSet.as_view({'get': 'list'})),
     path('<int:pk>/', views.EventViewSet.as_view(
         {'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'})),
 ]
