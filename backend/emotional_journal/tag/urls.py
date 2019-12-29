207dbbfa06ee16ba00ddf95aeee717c28833f19a
krishtna999 <krishtna999@gmail.com>
2019-12-29 18:21:38 +0530

    Fixed url endpoints in accordance with revamp standards

--- backend/emotional_journal/tag/urls.py
+++ backend/emotional_journal/tag/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import SimpleRouter
 from . import views
 
 router=SimpleRouter()
-router.register('event',views.TagViewSet)
+router.register('',views.TagViewSet)
 # print(router.urls)
 
 urlpatterns=router.urls
