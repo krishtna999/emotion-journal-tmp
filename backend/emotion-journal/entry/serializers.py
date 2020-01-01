758257c3457d47ec139aa803857ce0910ff18808
krishtna999 <krishtna999@gmail.com>
2020-01-02 02:52:43 +0530

    Now, tags will be displayed in the entry json

--- backend/emotion-journal/entry/serializers.py
+++ backend/emotion-journal/entry/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 
 from .models import Entry
 
+from event.serializers import EventSerializer
+
 class EntrySerializer(serializers.ModelSerializer):
+    events=EventSerializer(many=True)
     class Meta:
         model = Entry
         fields = ['id','datetime','events']
         depth=1
-
-
