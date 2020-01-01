b8f5ba3a8f6f3ceb177473e45b1fd0c0417ee9f1
krishtna999 <krishtna999@gmail.com>
2020-01-02 02:48:05 +0530

    Added event and event-display componenets

--- frontend/emotion-journal/src/entry/entry.module.ts
+++ frontend/emotion-journal/src/entry/entry.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SyncService } from '../app/sync.service';
 import { EntryDisplayComponent } from './entry-display/entry-display.component';
-
+import { EventModule } from '../event/event.module';
 
 
 @NgModule({
   declarations: [EntryDisplayComponent],
   imports: [
     CommonModule,
+    EventModule,
   ],
   exports:[
     EntryDisplayComponent,
---
+++ frontend/emotion-journal/src/event/event.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EventDisplayComponent } from './event-display/event-display.component';
+
+
+
+@NgModule({
+  declarations: [EventDisplayComponent],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    EventDisplayComponent
+  ],
+})
+export class EventModule { }
