b497b5bd1484c0fa90dee0e96a016540c35433eb
krishtna999 <krishtna999@gmail.com>
2020-01-03 04:24:31 +0530

    Changed date to a generic one

--- frontend/emotion-journal/src/entry/entry-display/entry-display.component.ts
+++ frontend/emotion-journal/src/entry/entry-display/entry-display.component.ts
@@ -2,18 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 import { EntryService } from '../entry.service';
 
+import { EventDisplayComponent } from '../../event/event-display/event-display.component';
+
 @Component({
   selector: 'app-entry-display',
   templateUrl: './entry-display.component.html',
   styleUrls: ['./entry-display.component.css']
 })
 export class EntryDisplayComponent implements OnInit {
-
+  selected_date='2019-12-28';
   constructor(private entryService: EntryService) { }
   json;
 
   ngOnInit() {
-    this.json = this.entryService.getEntryByDate('2019-12-28');
+    this.json = this.entryService.getEntryByDate(this.selected_date);
     this.json.subscribe(data => {
       console.log(data[0]);
     });
