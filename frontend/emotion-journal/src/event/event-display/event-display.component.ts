be5651f59c5cc24c284a6334f769388208a71d92
krishtna999 <krishtna999@gmail.com>
2020-01-03 04:25:29 +0530

    Added event component and functionalities

--- frontend/emotion-journal/src/event/event-display/event-display.component.ts
+++ frontend/emotion-journal/src/event/event-display/event-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event-display',
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-display.component.css']
 })
 export class EventDisplayComponent implements OnInit {
+  @Input() event: object;
 
   constructor() { }
 
+  remove_tag(tag) {
+    const index = this.event['tags'].indexOf(tag);
+    this.event['tags'].splice(index, 1);
+  }
   ngOnInit() {
   }
 
--- frontend/emotion-journal/src/event/event.module.ts
+++ frontend/emotion-journal/src/event/event.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventDisplayComponent } from './event-display/event-display.component';
-
-
+import { NbCardModule } from '@nebular/theme';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [EventDisplayComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NbCardModule,
+    MatChipsModule,
+    MatIconModule,
   ],
-  exports:[
+  exports: [
     EventDisplayComponent
   ],
 })
