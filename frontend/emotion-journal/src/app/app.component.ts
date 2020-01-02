8a8ad19ee8b9f4374a01d87fe65fcc1b6b9d3cf3
krishtna999 <krishtna999@gmail.com>
2020-01-03 04:23:47 +0530

    Removed unused import

--- frontend/emotion-journal/src/app/app.component.ts
+++ frontend/emotion-journal/src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 
-
-import {EntryDisplayComponent} from '../entry/entry-display/entry-display.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
