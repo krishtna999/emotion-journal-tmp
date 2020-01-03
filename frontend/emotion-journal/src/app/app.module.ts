57e95081f6f7dfb6316689389c7a5bd1676e459f
krishtna999 <krishtna999@gmail.com>
2020-01-03 19:13:41 +0530

    Added datepicker to root module

--- frontend/emotion-journal/src/app/app.module.ts
+++ frontend/emotion-journal/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { NbLayoutModule,NbThemeModule,NbSidebarModule,NbThemeService } from '@nebular/theme';
+import { NbLayoutModule,NbThemeModule,NbSidebarModule,NbDatepickerModule,NbThemeService } from '@nebular/theme';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +23,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     NbThemeModule.forRoot(),
     NbSidebarModule.forRoot(),
+    NbDatepickerModule.forRoot(),
     NbLayoutModule,
     EntryModule,
 
