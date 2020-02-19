95c9a375d5a01d6b847229fc7eb384dd8563e636
krishtna999 <krishtna999@gmail.com>
2020-02-19 16:49:33 +0530

    Added date to serializer

--- backend/emotion-journal/event/serializers.py
+++ backend/emotion-journal/event/serializers.py
@@ -5,8 +5,9 @@ from .models import Event
 class EventSerializer(serializers.ModelSerializer):
     # event_text=serializers.CharField(source='retrieve_event_text')
     # entry_id=serializers.IntegerField(source='entry.id')
+    date = serializers.DateTimeField(format="%A, %d %b %Y",source='entry.datetime')
     class Meta:
         model = Event
-        fields = ['id','text','order_id','tags']
-        read_only=('order_id',)
+        fields = ['id','date','text','order_id','tags',]
+        read_only=('order_id','date')
         depth=1
